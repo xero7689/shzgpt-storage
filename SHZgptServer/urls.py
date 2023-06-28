33b3ff1a2f86a13078796ee0eef6252c77fa3556
xero7689 <volleyp7689@gmail.com>
2023-06-28 15:21:18 +0800

    Update: Change root path of API

--- SHZgptServer/urls.py
+++ SHZgptServer/urls.py
@@ -21,6 +21,6 @@ from django.urls import path, include
 from django.conf import settings
 
 urlpatterns = [
-    path('api/', include('chat.urls')),
+    path('chat/', include('chat.urls')),
     path(settings.DJANGO_ADMIN_URL_PATH, admin.site.urls),
 ]
