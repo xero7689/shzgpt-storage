3c9e6672bc92a427d1790d7548f2eab737281e60
xero7689 <volleyp7689@gmail.com>
2023-08-31 19:46:27 +0800

    Update: Change the import position of django.setup() in asgi.py

--- SHZgptServer/asgi.py
+++ SHZgptServer/asgi.py
@@ -8,6 +8,10 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
 
 import os
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SHZgptServer.settings')
+django.setup()
 
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
@@ -16,8 +20,6 @@ from channels.auth import AuthMiddlewareStack
 from django.core.asgi import get_asgi_application
 
 from chat.routing import websocket_urlpatterns
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SHZgptServer.settings')
 
 django_asgi_app = get_asgi_application()
 
